09e662d8cdc1dc5900a62699661d165f0b9f3eea
todortodorov0 <todor1232001@gmail.com>
2020-10-14 13:43:46 +0000

    Exercise3

--- src/exercise3/AngleCalculation.java
+++ src/exercise3/AngleCalculation.java
@@ -11,10 +11,10 @@ public class AngleCalculation {
         char type= in.charAt(0);
         do{
             if(type=='d'){
-              double R=value*Math.PI/180/2;
+              double R=value*3.14/180/2;
                 System.out.println("Angel: "+ R);
             }else if(type=='r'){
-                double R=2*value*57.29578;
+                double R=2*value*57.29;
                 System.out.println("Angel: "+ R);
             }break;
         }while(type=='d' || type=='r');
