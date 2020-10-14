2e2ad38ddf51482cf1f2b4ba4448f1d29b7d80f3
todortodorov0 <todor1232001@gmail.com>
2020-10-14 13:20:10 +0000

    Exercise3

--- src/exercise3/NamePrinter.java
+++ src/exercise3/NamePrinter.java
@@ -8,7 +8,7 @@ public class NamePrinter {
         String name=input.next();
         for(int i=0;i<name.length();i++){
             char letter=name.charAt(i);
-            System.out.println("*"+letter+"*");
+            System.out.println("* "+letter+" *");
         }
 
     }
